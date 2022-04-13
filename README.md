637cb192193435d27d3cf2689b76cd73128c0215
frenzis01 <francesco.lorenzoni12042@gmail.com>
2022-04-13 11:57:27 +0200

    implements select. Proper testing needed

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Bridge
+# NetBridge
 Simple app in C which acts as a bridge between two network interfaces
--- bridge.c
+++ bridge.c
@@ -1,46 +1,94 @@
+/**
+ * @file bridge.c
+ * @author your name (you@domain.com)
+ * @brief usage: 
+ *  gcc bridge.c -o bridge -lpcap
+ *  bridge enp2s0f1 wlp3s0
+ * @version 0.1
+ * @date 2022-04-13
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include <pcap/pcap.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/select.h>
 
-int main() {
+void help() {
+  printf("Usage: sudo ./bridge.c <interface_1> <interface_2>\n");
+}
+void forward(pcap_t *from, pcap_t *to);
+
+
+int main(int argc, const char **argv) {
   char *device = NULL, c, *bpfFilter = NULL;
   char errbuf[PCAP_ERRBUF_SIZE];
-  int promisc = 1, snaplen = 1500;
+  int promisc = 1, snaplen = 1500;  // TODO get actual mtu dynamically
   pcap_t *i1, *i2;
 
-  if ((i1 = pcap_open_live("enp2s0f1", snaplen,
-                             promisc, 500, errbuf)) == NULL) {
+  if (argc != 3) {
+    help();
+    return -1;
+  }
+
+  if ((i1 = pcap_open_live(argv[1], snaplen,
+                           promisc, 500, errbuf)) == NULL) {
     printf("pcap_open_live: %s\n", errbuf);
     return (-1);
   }
-  if ((i2 = pcap_open_live("wlp3s0", snaplen,
+  if ((i2 = pcap_open_live(argv[2], snaplen,
                            promisc, 500, errbuf)) == NULL) {
     printf("pcap_open_live: %s\n", errbuf);
     return (-1);
   }
 
-  const unsigned char *pkt_data;
-  struct pcap_pkthdr *header;
-  int res;
-  pcap_t *from = i1, *to = i2;
+  int res,
+      fd1 = pcap_fileno(i1),
+      fd2 = pcap_fileno(i2),
+      fd_max = fd1 > fd2 ? fd1 : fd2;
+  ;
+
+  fd_set mask;
+  struct timeval timeout;
+  timeout.tv_sec = 1;  // wake from select every second
+  timeout.tv_usec = 0;
   while (1) {
-    from = from == i1 ? i2 : i1;
-    to = to == i1 ? i2 : i1;
-    if ((res = pcap_next_ex(from, &header, &pkt_data)) != 1) {
-      if (res == 0)
-        printf("pcap_next: timeout\n");
-      else
-        printf("pcap_next: some error\n");
-    } else {
-      printf("pcap_next: ok\n");
-      unsigned char *rec = (unsigned char*) pkt_data;
-      rec[0] = rec[1] = rec[2] = rec[3] = rec[4] = rec[5] = 0xa;
-      if ((res = pcap_sendpacket(to, pkt_data, header->caplen)) != 0)
-        printf("pcap_send: some error\n");
-      else
-        printf("pcap_send: ok\n");
+    // restore select set
+    FD_ZERO(&mask);
+    FD_SET(fd1, &mask);
+    FD_SET(fd2, &mask);
+    if (select(fd_max + 1, &mask, NULL, NULL, &timeout)) {
+      if (FD_ISSET(fd1, &mask))
+        forward(i1, i2);
+      if (FD_ISSET(fd2, &mask))
+        forward(i2, i1);
     }
   }
 
   return 0;
+}
+
+/**
+ * @brief Reads a packet from 'from' and forwards it to 'to'. Didn't expect that huh?
+ *
+ * @param from net interface to read a packet from
+ * @param to net interface to send the previously read packet to
+ */
+void forward(pcap_t *from, pcap_t *to) {
+  const unsigned char *pkt_data;
+  struct pcap_pkthdr *header;
+  int res;
+  if ((res = pcap_next_ex(from, &header, &pkt_data)) != 1) {
+    if (res == 0)
+      printf("pcap_next: timeout\n");
+    else
+      printf("pcap_next: some error\n");
+  } else {
+    printf("pcap_next: ok\n");
+    if ((res = pcap_sendpacket(to, pkt_data, header->caplen)) != 0)
+      printf("pcap_send: some error\n");
+    else
+      printf("pcap_send: ok\n");
+  }
 }
